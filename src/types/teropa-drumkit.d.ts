2ebc365a9a59b2608b18f5af7286f561beb3ba9a
Tom <tpepels@gmail.com>
2026-09-23 09:12:17 +0100

    Type sampled drum-kit asset imports

---
+++ src/types/teropa-drumkit.d.ts
@@ -0,0 +1,15 @@
+declare module "@teropa/drumkit" {
+  export const kick: string;
+  export const snare: string;
+  export const snare2: string;
+  export const snare3: string;
+  export const hatClosed: string;
+  export const hatClosed2: string;
+  export const hatOpen: string;
+  export const hatOpen2: string;
+  export const tomLow: string;
+  export const tomMid: string;
+  export const tomHigh: string;
+  export const ride: string;
+  export const crash: string;
+}
